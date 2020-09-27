b0ae5bc1d1f57ea812c1b2d263dba519ab176893
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-27 23:37:28 +0530

    Commit#18

--- toscrape.com/content_crawler.py
+++ toscrape.com/content_crawler.py
@@ -22,5 +22,20 @@ tag_list=[]
 for tags in soup.select('.tag-item a'):
     tag_list.append(tags.text)
 print(f"List of Top 10 tags on first page : \n\n {tag_list} \n\n")
+
+page_url_list = []
+for page_no in range(1,11):
+    page_url_list.append(f"http://quotes.toscrape.com/page/{page_no}/")
+#print(page_url_list)
+
+all_authors_list = []
+for page_url in page_url_list:
+    page_result = requests.get(page_url)
+    page_soup = bs4.BeautifulSoup(page_result.text,"lxml")
+    for author_name in page_soup.select('span small'):
+        all_authors_list.append(author_name.text)
+print(f"There are {len(list(set(all_authors_list)))} author names found on all pages of website quotes.toscrape.com : {set(all_authors_list)}")
+    
+        
     
 
