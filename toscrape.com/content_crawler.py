447abc5bf4291ae5b70d61d3dce54ebd60fd40d6
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2020-09-27 21:24:32 +0530

    Commit#16

--- toscrape.com/content_crawler.py
+++ toscrape.com/content_crawler.py
@@ -10,4 +10,11 @@ author_list = []
 for author in soup.select('span small'):
     author_list.append(author.text)
     
-print(f"List of authors on first page : \n\n {set(author_list)}")
+print(f"List of authors on first page : \n\n {set(author_list)} \n\n")
+
+quotes_list = []
+for quotes in soup.select('.text'):
+    quotes_list.append(quotes.text)
+print(f"List of quotes on first page : \n\n {quotes_list} \n\n")
+    
+
